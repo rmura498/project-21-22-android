d1e0b255d6f0848a9b4ed10941a752d817ab7db5
lucascionis <lucascionis@tiscali.it>
2022-07-15 12:10:13 +0200

    Updated "drebin.py"
    - added "create_vectorized_dataset" function to execute the code from "vectorizeDataset.py"
    - inserted simple check to verify if "dataset.json" already exists, to skip the analysis step

--- drebin.py
+++ drebin.py
@@ -1,11 +1,35 @@
+import os
+from os.path import join
+
 import createSingleDataset
 import readDataset
-import os
+from vectorizeDataset import vectorize_dataset
+from settings import DATASET
+from utils import load_json
 
-pr = os.fork()
-# TODO: check for exceptions here
-if pr == 0:
-    readDataset.run()
-else:
-    cpe = os.wait()
-    createSingleDataset.merge_json_files()
+
+def main():
+    dataset_path = join(DATASET, "dataset.json")
+    if not os.path.isfile(dataset_path):
+        pr = os.fork()
+        if pr == 0:
+            readDataset.run()
+        else:
+            cpe = os.wait()
+            createSingleDataset.merge_json_files()
+            create_vectorized_dataset()
+    else:
+        create_vectorized_dataset()
+
+
+def create_vectorized_dataset():
+    dataset_path = join(DATASET, "dataset.json")
+    dataset = load_json(dataset_path)
+
+    X = vectorize_dataset(dataset)
+    print("\n\n1/4 of sample1's vectorized features:\n")
+    print(X[1, :X.shape[1] // 4])
+
+
+if __name__ == '__main__':
+    main()
