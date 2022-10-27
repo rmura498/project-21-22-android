60f07e44fb8109be21ac6b038e9417d9e833a912
lucascionis <lucascionis@tiscali.it>
2022-10-27 20:33:13 +0200

    Added doc comments to 'utils.py'

--- Others/utils.py
+++ Others/utils.py
@@ -2,7 +2,11 @@ import ujson as json
 
 
 def load_json(filename):
-    # Load JSON file
+    """
+    Load a .json file.
+    :param filename: filename of the json file to load
+    :return: json_obj - the loaded json data structure
+    """
     with open(filename) as f:
         json_obj = json.load(f)
 
@@ -10,7 +14,13 @@ def load_json(filename):
 
 
 def save_json(path, content):
+    """
+    Save a data structure into a .json file.
+    :param path: where to save the json file
+    :param content: the content to be saved in the json
+    :return: None
+    """
     jsonFileName = path
-    jsonFile = open(jsonFileName, "a+")
+    jsonFile = open(jsonFileName, "w")
     jsonFile.write(json.dumps(content))
     jsonFile.close()
