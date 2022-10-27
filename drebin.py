cf2a4059b465bcf1be02194df2a3502fdb3724aa
lucascionis <lucascionis@tiscali.it>
2022-10-27 20:33:51 +0200

    Minor changes to drebin.py

--- drebin.py
+++ drebin.py
@@ -1,17 +1,16 @@
 from ApkDetectO.CDatasetProcessing import CDatasetProcessing
 from ApkDetectO.CClassification import CClassification
-# import joblib
 
 
 def main():
     dataset_processor = CDatasetProcessing()
-    pr = dataset_processor.process()
+    dataset_processor.process()
 
-    if pr == 1:
-        classification = CClassification(dataset_processor.X,
-                                         dataset_processor.y)
-        classification.run()
-        # clf = joblib.load("clf.pkl")
+    classification = CClassification(
+        dataset_processor.X,
+        dataset_processor.y
+    )
+    classification.run()
 
 
 if __name__ == '__main__':
